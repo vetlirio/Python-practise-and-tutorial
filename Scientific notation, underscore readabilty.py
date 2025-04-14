a3a59a42a7e8ed6f39c5ff963de0d39fd06efd39
vetlirio <vetle.pt@gmail.com>
2025-04-14 14:14:05 +0200

    Add files via upload

---
+++ Scientific notation, underscore readabilty.py
@@ -0,0 +1,21 @@
+# You can add the letter e or E followed by a positive or negative integer to specify that you're using scientific notation.
+
+print(16e3)
+# Prints 16000.0
+
+print(7.1e-2)
+# Prints 0.071
+
+
+
+Underscores for Readability
+
+Python also allows you to represent large numbers in the decimal format using underscores as the delimiter instead of commas to make it easier to read.
+
+num = 16_000
+print(num)
+# Prints 16000
+
+num = 16_000_000
+print(num)
+# Prints 16000000
