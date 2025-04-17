ee5b1fb1a6fc5e7bc55bed442e5278f8eead5682
vetlirio <vetle.pt@gmail.com>
2025-04-17 06:02:54 +0200

    Add files via upload

---
+++ Practising Functions.py
@@ -0,0 +1,15 @@
+# im gonna practise my function skills
+
+def greeting():
+    print("Hello there,")
+    
+greeting()
+print("Code-master, you're doing great!")
+
+
+
+#Can I get them on the same line?
+def greeting(name):
+    print("Hello there, " + name + ", you're doing great!")
+    
+greeting("Code-master")
