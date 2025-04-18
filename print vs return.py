e6611965f73205999e19244dd72b08c3007a64b2
vetlirio <vetle.pt@gmail.com>
2025-04-18 05:27:41 +0200

    Add files via upload

---
+++ print vs return.py
@@ -0,0 +1,9 @@
+    
+    """ print() is a function that:
+        Prints a value to the console
+        Does not return a value
+    return is a keyword that:
+        Ends the current function's execution
+        Provides a value (or values) back to the caller of the function
+        Does not print anything to the console (unless the return value is later print()ed)
+    """
