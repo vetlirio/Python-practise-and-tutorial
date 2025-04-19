0cc7f863f191156f2f0c6dbeaf188966f58c0bb6
vetlirio <vetle.pt@gmail.com>
2025-04-19 16:55:56 +0200

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,40 @@
+#🔹 Variable = "Here’s some data."
+
+#🔹 Function = "Here’s something I want you to do."
+
+#this one actually worked Iguess
+def two_extra_added(function):
+    two = 2
+    result = two + function
+    return result
+
+one_point = 1
+two_points = 2
+
+combined = two_extra_added(one_point)
+
+
+# another one from chat gpt
+print(combined)
+    
+def greet():
+    print("Hello!")
+
+greet()  # ➜ should print: Hello!
+greet()
+
+
+def say_name():
+    print("Code-master")
+    
+say_name()
+
+
+
+
+
+
+def say_name(blabla):
+    print("My name is", blabla)
+
+say_name("Kasper")
