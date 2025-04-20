55f8a920d2ea38ca0fa47f7132addc1e038890ac
vetlirio <vetle.pt@gmail.com>
2025-04-20 22:12:13 +0200

    Add files via upload

---
+++ Average score.py
@@ -0,0 +1,13 @@
+# Average score
+game_one_score = 97
+game_two_score = 92
+game_three_score = 106
+game_four_score = 105
+
+# Don't touch above this line
+
+average_score = ((97 + 92 + 106 + 105) / 4)
+
+# Don't touch below this line
+
+print(round(average_score))
