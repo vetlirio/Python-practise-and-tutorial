54bca7afa034d251a411f4a195901f0a82c0499b
vetlirio <vetle.pt@gmail.com>
2025-04-22 17:00:30 +0200

    Add files via upload

---
+++ Variables 1.py
@@ -0,0 +1,28 @@
+# A variable is just a name we give to a certain value
+# We have the freedom to choose any name for our variables,
+# but they should be descriptive and consist of a single "token", meaning continuous text with underscores separating the words.
+my_height = 193
+my_name = "Kristian"
+print(f"My name is {my_name} and Im a {my_height} cm tall.")
+
+
+# Variables are called "variables" because they can hold any value and that value can change (it varies). Below is the 
+
+player_health = 1000
+
+player_health = 900
+
+print(player_health)
+
+player_health = 800
+
+print(player_health)
+
+player_health = 700
+
+print(player_health)
+
+player_health = 600
+
+print(player_health)
+
