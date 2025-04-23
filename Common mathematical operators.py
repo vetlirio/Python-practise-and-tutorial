492769f2f413e995281805db90a15e05e5631053
vetlirio <vetle.pt@gmail.com>
2025-04-23 23:04:15 +0200

    Add files via upload

---
+++ Common mathematical operators.py
@@ -0,0 +1,38 @@
+# common mathematical operators in Python syntax.
+
+# summation = a + b  # Addition
+# difference = a - b # Subtraction
+# product = a * b    # Multiplication
+# quotient = a / b   # Division
+
+# Parentheses can be used to order math operations.
+# avg = (a + b + c) / 3
+
+# Negative numbers in Python work the way you probably expect. Just add a minus sign:
+# my_negative_num = -1
+player_health = 100
+poison_damage = -10
+
+player_poison_health = player_health + poison_damage
+
+print(player_poison_health)
+
+# example of Multiplication
+player_health = 1000
+armor_multiplier = 2
+
+armored_health  = player_health * armor_multiplier
+print(armored_health)
+
+
+# Multi-Line Comments (Aka docstrings)
+
+# You can use triple quotes to start and end multi-line comments as well:
+
+"""
+    the code found below
+    will print 'Hello, World!' to the console
+"""
+print("Hello, World!")
+
+# This is useful if you don't want to add the # to the start of each line when writing paragraphs of comments.
