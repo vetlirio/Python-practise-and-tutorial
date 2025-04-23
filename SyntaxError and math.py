2fcc707dc059fb51f42a906a1d438b9072434a9f
vetlirio <vetle.pt@gmail.com>
2025-04-23 23:05:21 +0200

    Add files via upload

---
+++ SyntaxError and math.py
@@ -0,0 +1,17 @@
+# simple math is common in code
+print(200 + 300)
+
+enchanted_sword = 325
+print(enchanted_sword)
+
+#syntax is jargon for valid code. If error occurs, syntaxError is when something issent valid code.
+#example of invalid code on line 9
+# print("welcome to dragon quest"]
+# it wouldnt work simply do to not having the right paranteses at the end.
+# print("welcome to dragon quest') wouldnt work even tho you use 1 or two quotation at start and end of a string, they must pair, like this: "...." or '....'
+print("welcome to dragon quest")
+#correct the syntaxError
+#print("(250 + 241 + 244 + 255) / 4")   Wy wouldt this code work? correct code on line 16.
+
+print((250 + 241 + 244 + 255) / 4)
+print("Because of the paranteses, python interpreted all the numbers and math symbols / as  just a string (str) (piece of text)")
