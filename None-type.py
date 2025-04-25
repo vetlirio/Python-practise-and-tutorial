8658d966fb758330acbc5e54e28e5394cfe0ddae
vetlirio <vetle.pt@gmail.com>
2025-04-26 00:58:49 +0200

    Add files via upload

---
+++ None-type.py
@@ -0,0 +1,9 @@
+# NoneType is not the same as a string with a value of "None":
+
+my_none = None # this is a None-type
+my_none = "None" # this is a string with the value "None"
+
+enemy = None
+# don't touch below this line
+print(enemy is None)
+
