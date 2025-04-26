dabba0491a4efd4314cd797df3395818d7bff0b3
vetlirio <vetle.pt@gmail.com>
2025-04-26 06:33:50 +0200

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+color = input("What color do you like? ")
+country = input("What country are you from? ")
+age = input("How old are you? ")
+
+
+
+print("Soooooo....your name is " + name + " and you are " + age + " years old. You come from " + country + " and for some reason you favorite color is " + color + ".....?")
+
+sure = input("are you sure? ")
+
+print("Okey, okey...relax, im just checking, buddy!")
