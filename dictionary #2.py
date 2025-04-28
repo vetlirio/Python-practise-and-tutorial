988131f6b438b4b1f4c3e7d7ed112dc9a65e67bd
vetlirio <vetle.pt@gmail.com>
2025-04-28 06:40:47 +0200

    Add files via upload

---
+++ dictionary #2.py
@@ -0,0 +1,15 @@
+# dictionary =  a collection of {key:value} pairs
+
+# ordered and changeable. No duplicates
+
+
+
+capitals = {"USA": "Washington D.C.",
+
+                    "India": "New Delhi",
+
+                    "China": "Beijing",
+
+                    "Russia": "Moscow"}
+
+print(capitals)
