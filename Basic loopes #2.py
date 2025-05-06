1155106ee2a98d3e6d80453ab6b88d4c0ab10fdb
vetlirio <vetle.pt@gmail.com>
2025-05-06 08:03:10 +0200

    Add files via upload

---
+++ Basic loopes #2.py
@@ -0,0 +1,33 @@
+num = 0
+while num < 3:
+    num += 1
+    print(num)
+
+
+
+num = 10  # start number 
+while num < 100: #stop number 
+    num += 10 # increase number 
+    print(num) #printing the while loop
+    
+    
+num= 0
+while num < 730:
+    num += 73
+    print(num)
+    
+"""
+
+
+
+
+"""
+    
+#num = 0
+while num < 1000:
+    num += 1
+    print(num)
+    
+
+    
+# , 5, 92, 18, 47, 66, 30, 81, 12, 59
