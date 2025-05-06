db104058a61c6cbab5783506a0b0ad8773b7f9f8
vetlirio <vetle.pt@gmail.com>
2025-05-06 08:02:56 +0200

    Add files via upload

---
+++ Basic loopes.py
@@ -0,0 +1,24 @@
+loop_1 = 0
+while loop_1 < 10:
+    loop_1 += 1
+    print(loop_1)
+    
+loop_2 = 10
+while loop_2 < 30:
+    loop_2 += 2
+    print(loop_2)
+    
+loop_3 = 30
+while loop_3 < 60:
+    loop_3 += 3
+    print(loop_3)
+    
+loop_4 = 60
+while loop_4 < 100:
+    loop_4 += 4
+    print(loop_4)
+    
+loop_5 = 100
+while loop_5 < 150:
+    loop_5 += 5
+    print(loop_5)
