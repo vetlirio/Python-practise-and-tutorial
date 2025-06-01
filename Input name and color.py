7ea2600021ba777d28dea123d8b2b1640ed524fc
vetlirio <vetle.pt@gmail.com>
2025-06-01 17:00:49 +0200

    Add files via upload

---
+++ Input name and color.py
@@ -0,0 +1,8 @@
+"""name = input("What is you name? ")
+print("Hi " + name)
+"""
+a_person = input("What is you name? ")
+
+color = input("What color do you like? ")
+
+print(a_person + " likes " + color)
